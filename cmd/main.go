11c8d4d6f090611bc1f4bb224c09f2158503125e
Sts <sxii@Mac-mini-Sts.local>
2026-03-02 00:16:05 +0300

    init http server

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type MyResponse struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func NewMyResponse() MyResponse {
+	return MyResponse{
+		Name: "Alex",
+		Age:  25,
+	}
+}
+
+func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	myResponse := NewMyResponse()
+	myResponseToByte, err := json.Marshal(&myResponse)
+	if err != nil {
+		fmt.Printf("Ошибка сериализации данных %v", err)
+	}
+	_, err = w.Write(myResponseToByte)
+	if err != nil {
+		fmt.Printf("Ошибка отправки данных %v", err)
+	}
+}
+
+func main() {
+	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ArticlesCategoryHandler)
+
+	fmt.Println("Server listening on port 8080...")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sxiixii/petgo
+
+go 1.25.0
